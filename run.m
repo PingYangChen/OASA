baee20bc305fafd2efeb97d93a479e17c8630501
PingYangChen <pychen.ping@gmail.com>
2016-11-02 20:50:25 -0700

    demo

--- run.m
+++ run.m
@@ -1,37 +1,23 @@
-clear all
-format long g
-% 
+% Initialize Target Design Settings
 DesignInfo = getDesignInfo();
-DesignInfo.nRuns = 36;
-DesignInfo.nFactors = 18;
-CoreSet = TwoFIstruct(3);
+% Set Number of Runs
+DesignInfo.nRuns = 36;			
+% Set Number of Factors
+DesignInfo.nFactors = 18;		
+% Choose Type of Two Factor Interaction
+CoreSet = TwoFIstruct(3);		
 DesignInfo.CoreFig = CoreSet(:,:,2); 
+% Get Hadamard Matrix Based on Run Size (Please visit http://neilsloane.com/hadamard/)
+MAT = ConvertHadamard(DesignInfo.nRuns, 'http://neilsloane.com/hadamard/had.36.will.txt');
+%MAT = ConvertHadamard(DesignInfo.nRuns, 'http://neilsloane.com/hadamard/had.36.pal2.txt'); % another Hadamard matrix
+DesignInfo.Hadamard = MAT(:,2:end); % the 1st column is for intercept term
 
-Row_1 = [1,1,-1,-1,1,1,1,1,-1,1,-1,1,-1,-1,-1,-1,1,1,-1];
-hadmat_20 = zeros(20, 20-1);
-for i = 1:20,
-	if i == 1,
-	hadmat_20(i,:) = Row_1;
-	elseif i == 20,
-	hadmat_20(i,:) = (-1).*ones(1,20-1);
-	else,
-	hadmat_20(i,:) = Row_1([i:(20-1), 1:(i-1)]);
-	end
-end
+% Note that, for 20 runs, we have four Hadamard matrices in our repository, for example,
+% DesignInfo.Hadamard = csvread('H20-Q.csv');
 
-%MAT = horzcat(ones(20,1), hadmat_20);
-%MAT = ConvertHadamard(DesignInfo.nRuns, 'http://neilsloane.com/hadamard/had.24.pal.txt');
-%MAT = ConvertHadamard(DesignInfo.nRuns, 'http://neilsloane.com/hadamard/had.28.will.txt');
-%MAT = ConvertHadamard(DesignInfo.nRuns, 'http://neilsloane.com/hadamard/had.28.pal2.txt');
-%MAT = ConvertHadamard(DesignInfo.nRuns, 'http://neilsloane.com/hadamard/had.32.pal.txt');
-%MAT = ConvertHadamard(DesignInfo.nRuns, 'http://neilsloane.com/hadamard/had.36.will.txt');
-MAT = ConvertHadamard(DesignInfo.nRuns, 'http://neilsloane.com/hadamard/had.36.pal2.txt');
-%MAT = ConvertHadamard(DesignInfo.nRuns, 'http://neilsloane.com/hadamard/had.52.will.txt');
-DesignInfo.Hadamard = MAT(:,2:end);
-%DesignInfo.Hadamard = csvread('H20-Q.csv');
-
-AlgInfo = getAlgInfo(); % Initialize Algorithm Settings
-AlgInfo.Iter = 50; % Total OASA Iterations
+% Initialize Algorithm Settings
+AlgInfo = getAlgInfo(); 
+AlgInfo.Iter = 50; 		% Total OASA Iterations
 
 % Settings for The Updating [D2,D3] Step
 AlgInfo.D3.Swarm = 8; % Swarm Size
@@ -54,6 +40,6 @@ AlgInfo.D1f3.qGB = 1; 		% Number of Exchanged Columns with Global Best
 AlgInfo.D1f3.qLB = 1; 		% Number of Exchanged Columns with Local Best
 AlgInfo.D1f3.qRC = 2; 		% Number of Exchanged Columns with Randomly Chosen Columns in Candidate Set
 
-
 % Run OASA
 output = OASA(DesignInfo, AlgInfo)
+
